7e66c4ff40c8371053a4e7fdcaff25f32dce319f
ZhangYao <1870476411@qq.com>
2025-11-30 03:38:57 +0800

    抽离公共方法hm-api

--- hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "com.hmall.cart.client")
+@EnableFeignClients(basePackages = "com.hmall.api.client")
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
